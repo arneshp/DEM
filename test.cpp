7291c943f5e7fd990efe120d690df206a8913223
arnesh <arneshpalanisamy@gmail.com>
2020-11-04 23:45:17 +0100

    added as sample program

---
+++ test.cpp
@@ -0,0 +1,21 @@
+/*sample cpp file */
+
+#include <iostream>
+#include<cstdio>
+#include<algorithm>
+#include<cstdlib>
+
+using namespace std;
+int main(){
+int a,b;
+	cout << "Hello World" <<endl;
+	cout << "Please enter number 1 "<<endl;
+	cin >>a;
+       cout <<"Please enter number 2" << endl;
+	cin >>b;
+ 	if (a>b)
+	cout <<"congrats you have won the lotter" <<endl;
+	else 
+	cout << "better luck next time"<< endl;
+}	
+
